3ad5bbc5f6ec6cc1f6a682cf376e0cd1f317e451
Jason Gmitter <jgmitter@cisco.com>
2019-02-25 10:05:21 -0500

    Update automate_nginx.sh

--- automate_nginx.sh
+++ automate_nginx.sh
@@ -25,3 +25,7 @@
 apt-get update -y && apt-get upgrade -y
 apt-get install -y nginx
 echo "Hello World from host" $HOSTNAME "!" | sudo tee -a /var/www/html/index.html
+
+
+
+sudo nginx -t && sudo service nginx reload
